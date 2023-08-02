b3e687ec533f0c5c58ece1aba9baf7f7f829d1ff
Nakul Thakur <thakurnakul119@gmail.com>
2023-08-02 16:03:30 +0530

    change

--- Backend/app.js
+++ Backend/app.js
@@ -11,7 +11,7 @@ app.use(cookieParser());
 //   credentials: true,
 //   }));
   app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Origin', 'https://tour-management-system-0qjl.onrender.com');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
