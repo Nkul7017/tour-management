ca83fe8c3d3f00d180fa3850ec66d59344b54638
Nakul Thakur <thakurnakul119@gmail.com>
2023-08-02 13:22:45 +0530

    db change

--- Backend/db/conn.js
+++ Backend/db/conn.js
@@ -1,6 +1,6 @@
 const mongoose=require('mongoose')
 console.log(process.env.DB)
-mongoose.connect(`mongodb://127.0.0.1:27017/${process.env.DB}`)
+mongoose.connect(`mongodb://mongodb+srv://thakurnakul119:qwertyuiop123%40@cluster0.k2dgdqj.mongodb.net//${process.env.DB}`)
 .then(()=>{
     console.log("Connected Successfully")
 })
